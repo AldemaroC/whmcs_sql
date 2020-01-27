8ee2baf73a6d15e32ba3f9a0350d447241c7dac2
Aldemaro Campos <marocampos94@gmail.com>
2020-01-27 14:54:19 -0300

    Update to PHP7

--- query.php
+++ query.php
@@ -1,25 +1,23 @@
 <?php
-		$db = $_POST['usuariocliente'];
-		$user = $_POST['usuariocliente'];
-		
-	
-	if ( $_POST['acao'] == "criar" ) {
-		
-		$dbpass = $_POST['senhacliente'];
-		
-		$con = mysql_connect($_POST['servidor'],$_POST['usuarioservidor'],$_POST['senhaservidor']);
+$db = $_POST['usuariocliente'];
+$user = $_POST['usuariocliente'];
+$dbpass = $_POST['senhacliente'];
 
-		mysql_query("CREATE DATABASE ".$db."",$con)or die(mysql_error());
-		mysql_query("GRANT ALL ON ".$db.".* to  ".$user." identified by '".$dbpass."'",$con) or die(mysql_error());
-		mysql_close($con);
-		
-	} elseif ( $_POST['acao'] == "destruir" ) {
-		
-		$con = mysql_connect($_POST['servidor'],$_POST['usuarioservidor'],$_POST['senhaservidor']);
-
-		mysql_query("DROP USER ".$db."",$con)or die(mysql_error());
-		mysql_query("DROP DATABASE IF EXISTS ".$db."",$con)or die(mysql_error());
-		mysql_close($con);
+//Connect to db
+$mysqli = new mysqli($_POST['servidor'],$_POST['usuarioservidor'],$_POST['senhaservidor']);
+//Test the connection
+	if ($mysqli -> connect_errno) {
+	  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+	  exit();
 	}
-		
+//Create service
+if ( $_POST['acao'] == "criar" ) {
+	$mysqli -> query("CREATE DATABASE ".$db."");
+	$mysqli -> query("GRANT ALL ON ".$db.".* to  ".$user." identified by '".$dbpass."'");
+//Terminate service
+} elseif ( $_POST['acao'] == "destruir" ) {
+	$mysqli -> query("DROP USER ".$db."");
+	$mysqli -> query("DROP DATABASE IF EXISTS ".$db."");
+}
+$mysqli -> close();
 ?>
